01d30516ddca010b2e28ec69395fff7b593756ed
Archelaus <dragommer@gmail.com>
2012-02-18 23:11:23 +0000

    Added "FireBand"

--- src/com/gentoomen/ancientrelics/enchantments/EnchantmentFireBrand.java
+++ src/com/gentoomen/ancientrelics/enchantments/EnchantmentFireBrand.java
@@ -1,6 +1,12 @@
 package com.gentoomen.ancientrelics.enchantments;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.Vector;
 
 import com.gentoomen.ancientrelics.baseobjects.EnchantmentRelic;
 
@@ -9,9 +15,16 @@ public class EnchantmentFireBrand extends EnchantmentRelic{
 	@Override
 	public void excute(Event event) {
 		super.excute(event);
-		
+		PlayerInteractEvent playerEvent = (PlayerInteractEvent) event;
+		if(playerEvent.getClickedBlock() != null){
+			World world = playerEvent.getPlayer().getWorld();
+			Block aboveBlock = playerEvent.getPlayer().getWorld().getBlockAt(playerEvent.getClickedBlock().getLocation().add(new Location(world, 0, 1, 0)));
+			if(!(aboveBlock.getType() == Material.AIR)){
+				return;
+			}
+			aboveBlock.setType(Material.FIRE);
+		}
 	}
-	
 	
 	
 }
